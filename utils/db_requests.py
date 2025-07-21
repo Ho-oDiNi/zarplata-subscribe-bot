1d69ed3cc5fa083af58f701039afa358e4222628
Evgeny Irikov <Evgen5060@gmail.com>
2025-07-21 14:21:57 +0500

    refactor(db): изменил id таблиц

--- utils/db_requests.py
+++ utils/db_requests.py
@@ -4,7 +4,7 @@ def add_or_update_user(user_id):
     cursor = DB.cursor()
     
     cursor.execute('''
-    INSERT OR REPLACE INTO users (user_id)
+    INSERT OR REPLACE INTO users (id)
     VALUES (?)
     ''', (user_id,))
     
@@ -15,7 +15,7 @@ def subscribe_to_category(user_id, target_slug):
     cursor = DB.cursor()
     
     # Check if category exists
-    cursor.execute('SELECT category_id FROM categories WHERE category_slug = ?', (target_slug,))
+    cursor.execute('SELECT id FROM categories WHERE slug = ?', (target_slug,))
     category_id = cursor.fetchone()[0]
     if not category_id:
         cursor.close()
@@ -28,7 +28,7 @@ def subscribe_to_category(user_id, target_slug):
     ''', (user_id, category_id,))
     
     # Get category name for response
-    cursor.execute('SELECT category_name FROM categories WHERE category_id = ?', (category_id,))
+    cursor.execute('SELECT name FROM categories WHERE id = ?', (category_id,))
     category_name = cursor.fetchone()[0]
     
     DB.commit()
@@ -40,7 +40,7 @@ def subscribe_to_author(user_id, target_slug):
     cursor = DB.cursor()
     
     # Check if authors exists
-    cursor.execute('SELECT author_id FROM authors WHERE author_slug = ?', (target_slug,))
+    cursor.execute('SELECT id FROM authors WHERE slug = ?', (target_slug,))
     author_id = cursor.fetchone()[0]
     if not author_id:
         cursor.close()
@@ -53,7 +53,7 @@ def subscribe_to_author(user_id, target_slug):
     ''', (user_id, author_id,))
     
     # Get category name for response
-    cursor.execute('SELECT author_name FROM authors WHERE author_id = ?', (author_id,))
+    cursor.execute('SELECT name FROM authors WHERE id = ?', (author_id,))
     author_name = cursor.fetchone()[0]
     
     DB.commit()
